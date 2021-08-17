8f6e1bd9108ba724fe1ca5e84ecef4f9eb2b0fa6
yosefcode <67313446+yosefcode@users.noreply.github.com>
2021-08-17 04:03:38 +0300

    456

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(bodyParser.json());
 const dotenv = require("dotenv");
 
 dotenv.config();
-app.use(express.static(path.join(__dirname, "build")));
+// app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 const PORT = process.env.PORT || 7000;
 
@@ -25,9 +26,9 @@ connectToDb().then(async () => {
   });
 });
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 app.get("/api/list/", async (req, res) => {
   const apartments = await models.apartmentSchema.find();

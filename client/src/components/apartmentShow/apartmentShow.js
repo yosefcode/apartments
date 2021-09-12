06489478164da693723d7ef67f8b6f1adeda6866
yosefcode <67313446+yosefcode@users.noreply.github.com>
2021-09-12 20:25:43 +0300

    ss

--- client/src/components/apartmentShow/apartmentShow.js
+++ client/src/components/apartmentShow/apartmentShow.js
@@ -1,99 +1,94 @@
-// import "./apartmentShow.css";
-// import React, { useState, useEffect } from "react";
-// import { useParams } from "react-router-dom";
-// import axios from "axios";
+import "./apartmentShow.css";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Carousel from "react-gallery-carousel";
+import "react-gallery-carousel/dist/index.css";
+
+function ApartmentShow() {
+  const { id } = useParams();
+
+  const [apartmentShow, setApartmentShow] = useState([]);
+
+  useEffect(() => {
+    axios.post("/api/list/" + id).then((res) => {
+      setApartmentShow(res.data);
+    });
+  }, [id]);
+
+  const firstImage = apartmentShow.map((list) => list.firstImage);
+
+  const images = apartmentShow.map((list) => list.images.map((image) => image));
+
+  const allImages = firstImage.concat(images[0]);
+
+  const carouselImages = allImages.map((images) => ({
+    src: images,
+  }));
+
+  return (
+    <div className="description">
+      <div>
+        {" "}
+        {apartmentShow.map((list) => (
+          <div key={list._id}>
+            <div dir="ltr">
+              <Carousel
+                isLoop={true}
+                isAutoPlaying={true}
+                autoPlayInterval={5000}
+                transitionDurationMin={1000}
+                // transitionSpeed={1000}
+                // transitionDurationLimit={1000}
+                // // hasIndexBoard={false}
+                hasMediaButton={false}
+                hasSizeButton="bottomRight"
+                // hasThumbnails="bottomRight"
+                images={carouselImages}
+                style={{ height: 500, width: 800 }}
+              />
+            </div>
+
+            <div>
+              דירת {list.rooms} חדרים - ב{list.city}
+              <br />
+              עד {list.beds} מיטות
+              <br />
+              {list.long}
+            </div>
+            <div>
+              החל מ{list.price} ש"ח ללילה
+              <br />
+              {list.phone}
+            </div>
+          </div>
+        ))}{" "}
+      </div>
+    </div>
+  );
+}
+
+export default ApartmentShow;
+
+// import React from "react";
 // import Carousel from "react-gallery-carousel";
+// import aa from "./aa.png";
+// import ss from "./ss.png";
+// import dd from "./dd.png";
 // import "react-gallery-carousel/dist/index.css";
 
-// function ApartmentShow() {
-//   const { id } = useParams();
+// const App = () => {
+//   const aaa = [aa, ss, dd];
 
-//   const [apartmentShow, setApartmentShow] = useState([]);
-
-//   useEffect(() => {
-//     axios.post("/api/list/" + id).then((res) => {
-//       setApartmentShow(res.data);
-//     });
-//   }, [id]);
-
-//   const firstImage = apartmentShow.map((list) => list.firstImage);
-
-//   const images = apartmentShow.map((list) => list.images.map((image) => image));
-
-//   const allImages = firstImage.concat(images[0]);
-
-//   const imagesss = allImages.map((number) => ({
+//   const images = aaa.map((number) => ({
 //     src: number,
 //   }));
 
 //   return (
-//     <div className="description">
-//       <div>
-//         {" "}
-//         {apartmentShow.map((list) => (
-//           <div key={list._id}>
-//             <div dir="ltr">
-//               <Carousel
-//                 // isLoop={true}
-//                 // // hasIndexBoard={false}
-//                 // hasMediaButton={false}
-//                 // hasSizeButton="bottomRight"
-//                 // hasThumbnails="bottomRight"
-//                 // shouldMinimizeOnSwipeDown={false}
-//                 images={imagesss}
-//                 style={{ height: 500, width: 800 }}
-//               >
-//                 {/* {allImages.map((images, index) => (
-//                 <img
-//                   key={index}
-//                   src={images}
-//                   alt=""
-//                   style={{ height: 500, width: 800 }}
-//                 ></img>
-//               ))} */}
-//               </Carousel>
-//             </div>
-
-//             <div>
-//               דירת {list.rooms} חדרים - ב{list.city}
-//               <br />
-//               עד {list.beds} מיטות
-//               <br />
-//               {list.long}
-//             </div>
-//             <div>
-//               החל מ{list.price} ש"ח ללילה
-//               <br />
-//               {list.phone}
-//             </div>
-//           </div>
-//         ))}{" "}
-//       </div>
+//     <div dir="ltr">
+//       <Carousel images={images} style={{ height: 500, width: 800 }} />
 //     </div>
 //   );
-// }
+// };
 
-// export default ApartmentShow;
-
-import React from "react";
-import Carousel from "react-gallery-carousel";
-import aa from "./aa.png";
-import ss from "./ss.png";
-import dd from "./dd.png";
-import "react-gallery-carousel/dist/index.css";
-
-const App = () => {
-  const aaa = [aa, ss, dd];
-
-  const images = aaa.map((number) => ({
-    src: number,
-  }));
-
-  return (
-    <div dir="ltr">
-      <Carousel images={images} style={{ height: 500, width: 800 }} />
-    </div>
-  );
-};
-
-export default App;
+// export default App;

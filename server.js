ec1e2d6154fb997be1030dc4bfb38494fb1d335a
yosefcode <67313446+yosefcode@users.noreply.github.com>
2021-09-12 18:00:38 +0300

    aaa

--- server.js
+++ server.js
@@ -26,9 +26,10 @@ connectToDb().then(async () => {
   });
 });
 
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client/build", "index.html"));
-// });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 // app.get("/:id", (req, res) => {
 //   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 // });
